c4c7dd0d3019121f161b73676aa3033cdf385e36
Aboodipo <a730114257@gmail.com>
2025-07-27 01:49:49 +0300

    Create sample_config.py

---
+++ Tepthon/ Config/sample_config.py
@@ -0,0 +1,10 @@
+class Config:
+    API_ID = ""
+    API_HASH = ""
+    BOT_TOKEN = ""
+    HEROKU_API_KEY = None
+    HEROKU_APP_NAME = None
+    PM_LOGGER_GROUP_ID = -1001234567890
+    COMMAND_HAND_LER = "."
+    BOTLOG = True
+    ALIVE_NAME = "عبودثون"
