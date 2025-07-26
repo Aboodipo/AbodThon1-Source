987c2152e0f20aa2169d0eb970d79f4d502acb59
Aboodipo <a730114257@gmail.com>
2025-07-27 02:08:42 +0300

    Update sample_config.py

--- Tepthon/ Config/sample_config.py
+++ Tepthon/ Config/sample_config.py
@@ -1,7 +1,8 @@
 class Config:
-    API_ID = ""
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = ""           # ضع هنا API ID الخاص بك من Telegram
+    API_HASH = ""         # ضع هنا API HASH الخاص بك من Telegram
+    BOT_TOKEN = ""        # توكن بوت تلغرام
+    STRING_SESSION = ""   # جلسة المستخدم (إذا مستخدم بوت)
     HEROKU_API_KEY = None
     HEROKU_APP_NAME = None
     PM_LOGGER_GROUP_ID = -1001234567890
